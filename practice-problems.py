8eef76cec776d3d7fe524288ec717dfd6cd4fb8b
Jessica Dea <jessicardea@gmail.com>
2023-01-04 23:49:07 -0800

    Leetcode: Group Anagrams wip

--- practice-problems.py
+++ practice-problems.py
@@ -1,10 +1,14 @@
-def solution(s):
-    output = ""
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        output = [[strs[0]]]
 
-    for char in s:
-        if char.isupper():
-            output += " " + char
-        else:
-            output += char
+        for i in range(1, len(strs)):
+            for array in output:
+                if sorted(array[0]) == sorted(strs[i]):
+                    array.append(strs[i])
+                    break
+                elif array == output[-1]:
+                    output.append([strs[i]])
+                    break
 
-    return output
+        return output
