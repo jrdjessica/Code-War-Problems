cf1d8d9064d1b0200ccffacb2f8f0734b04f3c6d
Jessica Dea <jessicardea@gmail.com>
2023-01-09 23:45:53 -0800

    Valid braces

--- practice-problems.py
+++ practice-problems.py
@@ -1,13 +1,2 @@
-def alphabet_position(text):
-    import string
-
-    output = []
-    alphabet = list(string.ascii_lowercase)
-
-    for char in text:
-        if char.isalpha():
-            for i, letter in enumerate(alphabet):
-                if char.lower() == letter:
-                    output.append(str(i + 1))
-
-    return " ".join(output)
+def valid_braces(string):
+    pass
